c9aa00a683e09e4b3ec406c7436af1ca8b2afad4
mezzexmaz@outlook.com <mezzexmaz@outlook.com>
2024-07-06 15:03:54 +0530

    update and Get ServertTime

---
+++ EyeMezzexz/Controllers/ServerTimeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace EyeMezzexz.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServerTimeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetServerTime()
+        {
+            var serverTimeUtc = DateTime.UtcNow;
+            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+            var serverTimeIst = TimeZoneInfo.ConvertTimeFromUtc(serverTimeUtc, timeZoneInfo);
+
+            return Ok(new
+            {
+                ServerTimeIst = serverTimeIst
+            });
+        }
+    }
+}
